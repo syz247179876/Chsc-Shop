a466cef8da872a27ae9b38fa83fc1371e9965b7f
syz247179876 <247179876@qq.com>
2021-02-20 22:21:50 +0800

    完善历史搜索记录API和获取API

--- search_app/apis/search_api.py
+++ search_app/apis/search_api.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 from Emall.exceptions import ESConnectError
 from Emall.loggings import Logging
 from search_app.serailaizers.shop_search_serializers import CommoditySerializer
-from search_app.tasks import record_user_search
+from search_app.signals import record_search
 from search_app.utils.elasticsearch import ElasticSearchOperation
 from search_app.utils.pagination import CommodityResultsSetPagination
 from shop_app.models.commodity_models import Commodity
@@ -56,7 +56,7 @@ class CommoditySearchOperation(GenericAPIView):
                 serializer = self.get_serializer(page, many=True)
                 return self.get_paginated_response(serializer.data)
             serializer = self.get_serializer(queryset, many=True)
-            self.send_record_signal(request)  # 异步任务发送信号,记录用户浏览记录
+            self.send_record_signal(request)  # 异步任务发送信号,记录用户搜索记录
 
         except ConnectionError:
             # ES服务连接超时
@@ -64,7 +64,7 @@ class CommoditySearchOperation(GenericAPIView):
         return Response(serializer.data)
 
     @identity
-    def send_record_signal(self, unique_identity):
-        """发送记录历史记录信号"""
-        record_user_search.delay(unique_identity, self.request.query_params.copy().get('text'))
+    def send_record_signal(self, request, unique_identity):
+        """发送记录历史搜索记录信号"""
+        record_search.send(sender=unique_identity, request=request, keyword=self.request.query_params.copy().get('text'))
 
--- search_app/tasks.py
+++ search_app/tasks.py
@@ -8,7 +8,6 @@ import datetime
 from Emall import celery_apps as app
 from Emall import manage_redis
 from search_app.redis.history_redis import history_redis
-from search_app.signals import record_search
 
 
 @app.task
@@ -17,11 +16,5 @@ def timer_eliminate_heat():
     with manage_redis('search', type(history_redis)) as redis:
         date = datetime.datetime.today() - datetime.timedelta(1)
         redis.delete(history_redis.heat_key(date))
-
-
-@app.task
-def record_user_search(user_id, keyword):
-    """记录用户搜索记录"""
-    record_search.send(sender=user_id, keyworkd=keyword)
 
 
